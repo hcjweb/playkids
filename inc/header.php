5f1503225f3712bc4761e39aca158b538ce67eb9
hdcjunior <heliodacostajunior@gmail.com>
2016-01-06 20:33:35 -0200

    new style for header,nav,slide

--- inc/header.php
+++ inc/header.php
@@ -1,58 +1,37 @@
 
-
+    <div class="header" id="home">
   <div class="logo">
       <a href="index.html"><img src="images/logo.png" alt=""></a>
     </div>
-    <header class="contact-header">
-      <div class="phone">
-        <div class="cnt-left">
-        <span class="ads"> </span>
-      </div>
-      <div class="cnt-right">
-        <p>Rua Rio Grande do Sul, 363 Alumínio - SP</p>
-      </div>
-    </div>
-      <div class="clearfix"></div>
-      <div class="phone">
-        <div class="cnt-left">
-              <span class="ph"> </span>
-            </div>
-            <div class="cnt-right">
-              <p>11 95313-1480</p>
-            </div>
+        <div class="navigation">
+         <span class="menu"></span>
+          <ul class="navig">
+            <li><a href="index.php">Início</a><span> </span></li>
+            <li><a href="about.php">Sobre</a><span> </span></li>
+            <li><a href="gallery.php">Galeria</a><span> </span></li>
+            <li><a href="contact.php">Contato</a><span> </span></li>
+          </ul>
+        </div>
+    <div class="contact-header">
+      <section class="fone">
+        <img src="" alt="" />
+        <p><a href="contact.php">11 95313-1480</a></p>
+      </section>
+      <section class="mail">
+        <img src="" alt="" />
+        <p><a href="contact.php">helio@hcjweb.com.br</a></p>
+      </section>
+      <section class="endereco">
+        <img src="" alt="" />
+        <p><a href="contact.php">Rua Rio Grande do Sul, 363 Alumínio - SP</a></p>
+      </section>
+</div>
+
+
+
             <div class="clearfix"></div>
-          </div>
-          <div class="phone">
-            <div class="cnt-left">
-              <span class="email"> </span>
-            </div>
-            <div class="cnt-right">
-              <p><a href="mailto:example@email.com">helio@hcjweb.com.br</a></p>
-            </div>
-            <div class="clearfix"></div>
-          </div>
-        </header>
 
 
-<div class="header" id="home">
-  <div class="container">
 
-    <div class="navigation">
-     <span class="menu"></span>
-      <ul class="navig">
-        <li><a href="index.php">Início</a><span> </span></li>
-        <li><a href="about.php">Sobre</a><span> </span></li>
-        <li><a href="gallery.php">Galeria</a><span> </span></li>
-        <li><a href="contact.php">Contato</a><span> </span></li>
-      </ul>
-    </div>
-       <!-- script-for-menu -->
-   <script>
-      $("span.menu").click(function(){
-        $(" ul.navig").slideToggle("slow" , function(){
-        });
-      });
-   </script>
-   <!-- script-for-menu -->
-  </div>
+
 </div>
