fcd5725cd280634aa9adcd447ceeb350a33a47a5
hdcjunior <heliodacostajunior@gmail.com>
2016-01-06 22:02:24 -0200

    new style for header-recovery

--- inc/header.php
+++ inc/header.php
@@ -1,27 +1,37 @@
 
-
+    <div class="header" id="home">
   <div class="logo">
       <a href="index.html"><img src="images/logo.png" alt=""></a>
     </div>
-<div class="header" id="home">
-  <div class="container">
+        <div class="navigation">
+         <span class="menu"></span>
+          <ul class="navig">
+            <li><a href="index.php">Início</a><span> </span></li>
+            <li><a href="about.php">Sobre</a><span> </span></li>
+            <li><a href="gallery.php">Galeria</a><span> </span></li>
+            <li><a href="contact.php">Contato</a><span> </span></li>
+          </ul>
+        </div>
+    <div class="contact-header">
+      <section class="fone">
+        <img src="" alt="" />
+        <p><a href="contact.php">11 95313-1480</a></p>
+      </section>
+      <section class="mail">
+        <img src="" alt="" />
+        <p><a href="contact.php">helio@hcjweb.com.br</a></p>
+      </section>
+      <section class="endereco">
+        <img src="" alt="" />
+        <p><a href="contact.php">Rua Rio Grande do Sul, 363 Alumínio - SP</a></p>
+      </section>
+</div>
 
-    <div class="navigation">
-     <span class="menu"></span>
-      <ul class="navig">
-        <li><a class="active" href="index.html">Início</a><span> </span></li>
-        <li><a href="about.html">Sobre</a><span> </span></li>
-        <li><a href="gallery.html">Galeria</a><span> </span></li>
-        <li><a href="contact.html">Contato</a><span> </span></li>
-      </ul>
-    </div>
-       <!-- script-for-menu -->
-   <script>
-      $("span.menu").click(function(){
-        $(" ul.navig").slideToggle("slow" , function(){
-        });
-      });
-   </script>
-   <!-- script-for-menu -->
-  </div>
+
+
+            <div class="clearfix"></div>
+
+
+
+
 </div>
