c943c182238806d892b7fa837ce842a70b6b5f9f
hdcjunior <heliodacostajunior@gmail.com>
2016-01-13 10:13:55 -0200

    set gallery

--- gallery.php
+++ gallery.php
@@ -1,151 +1,106 @@
 
 <!DOCTYPE html>
- <html lang="pt-br">
- <head>
- <title>Play-Kids Eventos - Decorando Emoções</title>
- <meta name="viewport" content="width=device-width, initial-scale=1">
- <meta charset="utf-8">
- <meta name="keywords" content="" >
- <meta name="description" content="" >
- <script type="application/x-javascript"> addEventListener("load", function() {
-setTimeout(hideURLbar, 0); }, false); function hideURLbar(){
-window.scrollTo(0,1); } </script>
- <link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
- <link href="css/style.css" rel='stylesheet' type='text/css' />
- <link href='http://fonts.googleapis.com/css?family=Exo:100,200,300,400,500,600,700,800,900,100italic,200italic,300italic,400italic,500italic,600italic,700italic,800italic,900italic'
-rel='stylesheet' type='text/css'>
- <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic'
-rel='stylesheet' type='text/css'>
- <script src="js/jquery-1.11.0.min.js"></script>
-<!--start-smoth-scrolling-->
-<script type="text/javascript" src="js/move-top.js"></script>
-<script type="text/javascript" src="js/easing.js"></script>
-<script type="text/javascript">
-			jQuery(document).ready(function($) {
-				$(".scroll").click(function(event){
-					event.preventDefault();
-					$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
-				});
-			});
-		</script>
+<html>
+<head>
+<title>Play-Kids Eventos - Decorando Emoções</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Dream Wedding Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template,
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='http://fonts.googleapis.com/css?family=Exo:100,200,300,400,500,600,700,800,900,100italic,200italic,300italic,400italic,500italic,600italic,700italic,800italic,900italic' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
 
-<!--start-smoth-scrolling-->
+
+
+<!------ Light Box ------>
+<script type="text/javascript" src="js/jquery-1.9.0.min.js"></script>
+<link rel="stylesheet" href="css/swipebox.css">
+<link rel="stylesheet" href="css/galeria.css">
+<script src="js/ios-orientationchange-fix.js"></script>
+<script src="js/jquery.swipebox.min.js"></script>
+<script type="text/javascript">
+		jQuery(function($) {
+			$(".swipebox").swipebox();
+		});
+	</script>
+	<!------ Eng Light Box ------>
 
 </head>
 <body>
-	<!--start-header-->
-  <?php include 'inc/header.php'; ?>
+	<!----start-header---->
 
-	<!--end-header-->
+<?php include 'inc/header.php'; ?>
+
+	<!----end-header---->
 	<!--banner-starts-->
-	<div class="clearfix"> </div>
+		<div class="clearfix"> </div>
 
 							<img src="images/bnr-4.jpg" alt="">
 
 
 
 		<div class="clearfix"> </div>
-	<!----end-banner---->
-	<!----start-gallery---->
-	<div class="gallery">
-		<div class="container">
-			<div class="gallery-top heading">
-				<h3>Galeria de Imagens</h3>
+
+
+	<!----start-blog---->
+	 <div class="main">
+	 <div class="wrap">
+		<div class="products">
+			 <div class="top-box">
+				<h2>Galeria de Imagens</h2>
+				<hr>
+
+			 </div>
+
+		    <div class="section group">
+				<div class="col_1_of_4 span_1_of_4">
+					<a href="images/1/1.jpg" class="swipebox" title=""> <img src="images/1/thumbs/1.jpg" alt=""><span class="zoom-icon"></span> </a>
+
+                </div>
+				<div class="col_1_of_4 span_1_of_4">
+					<a href="images/1/2.jpg" class="swipebox" title=""> <img src="images/1/thumbs/2.jpg" alt=""><span class="zoom-icon"></span> </a>
+
+                </div>
+				<div class="col_1_of_4 span_1_of_4">
+				   <a href="images/1/3.jpg" class="swipebox" title=""> <img src="images/1/thumbs/3.jpg" alt=""><span class="zoom-icon"></span> </a>
+
+                </div>
+				<div class="col_1_of_4 span_1_of_4">
+					<a href="images/1/4.jpg" class="swipebox" title=""> <img src="images/1/thumbs/4.jpg" alt=""><span class="zoom-icon"></span> </a>
+
+                </div>
+				<div class="clear"></div>
 			</div>
-			<div class="gallery-bottom">
-				<ul id="og-grid" class="og-grid">
-					<li>
-						<a href="single.html" data-largesrc="images/1.jpg" data-title="Mauris accumsan" data-description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque malesuada purus a convallis dictum. Phasellus sodales varius diam, non sagittis lectus. Morbi id magna ultricies ipsum condimentum scelerisque vel quis felis.. Donec et purus nec leo interdum sodales nec sit amet magna. Ut nec suscipit purus, quis viverra urna.">
-							<img class="img-responsive" src="images/thumbs/1.jpg" alt="img01"/>
-						</a>
-					</li>
-					<li>
-						<a href="" data-largesrc="images/2.jpg" data-title="Vivamus mauris" data-description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque malesuada purus a convallis dictum. Phasellus sodales varius diam, non sagittis lectus. Morbi id magna ultricies ipsum condimentum scelerisque vel quis felis.. Donec et purus nec leo interdum sodales nec sit amet magna. Ut nec suscipit purus, quis viverra urna.">
-							<img class="img-responsive" src="images/thumbs/2.jpg" alt="img02"/>
-						</a>
-					</li>
-					<li>
-						<a href="" data-largesrc="images/3.jpg" data-title="Aliquam fringilla"  data-description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque malesuada purus a convallis dictum. Phasellus sodales varius diam, non sagittis lectus. Morbi id magna ultricies ipsum condimentum scelerisque vel quis felis.. Donec et purus nec leo interdum sodales nec sit amet magna. Ut nec suscipit purus, quis viverra urna.">
-							<img class="img-responsive" src="images/thumbs/3.jpg" alt="img03"/>
-						</a>
-					</li>
-					<li>
-						<a href="" data-largesrc="images/4.jpg" data-title="Nam lacinia"  data-description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque malesuada purus a convallis dictum. Phasellus sodales varius diam, non sagittis lectus. Morbi id magna ultricies ipsum condimentum scelerisque vel quis felis.. Donec et purus nec leo interdum sodales nec sit amet magna. Ut nec suscipit purus, quis viverra urna.">
-							<img class="img-responsive" src="images/thumbs/4.jpg" alt="img01"/>
-						</a>
-					</li>
-					<li>
-						<a href="" data-largesrc="images/5.jpg" data-title="Integer laoreet"  data-description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque malesuada purus a convallis dictum. Phasellus sodales varius diam, non sagittis lectus. Morbi id magna ultricies ipsum condimentum scelerisque vel quis felis.. Donec et purus nec leo interdum sodales nec sit amet magna. Ut nec suscipit purus, quis viverra urna.">
-							<img class="img-responsive" src="images/thumbs/5.jpg" alt="img01"/>
-						</a>
-					</li>
-					<li>
-						<a href="" data-largesrc="images/6.jpg" data-title="Quisque feugiat"  data-description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque malesuada purus a convallis dictum. Phasellus sodales varius diam, non sagittis lectus. Morbi id magna ultricies ipsum condimentum scelerisque vel quis felis.. Donec et purus nec leo interdum sodales nec sit amet magna. Ut nec suscipit purus, quis viverra urna.">
-							<img class="img-responsive" src="images/thumbs/6.jpg" alt="img02"/>
-						</a>
-					</li>
-					<li>
-						<a href="" data-largesrc="images/7.jpg" data-title="Aliquam eget"  data-description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque malesuada purus a convallis dictum. Phasellus sodales varius diam, non sagittis lectus. Morbi id magna ultricies ipsum condimentum scelerisque vel quis felis.. Donec et purus nec leo interdum sodales nec sit amet magna. Ut nec suscipit purus, quis viverra urna.">
-							<img class="img-responsive" src="images/thumbs/7.jpg" alt="img03"/>
-						</a>
-					</li>
-					<li>
-						<a href="" data-largesrc="images/8.jpg" data-title="Nullam ligula" data-description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque malesuada purus a convallis dictum. Phasellus sodales varius diam, non sagittis lectus. Morbi id magna ultricies ipsum condimentum scelerisque vel quis felis.. Donec et purus nec leo interdum sodales nec sit amet magna. Ut nec suscipit purus, quis viverra urna.">
-							<img class="img-responsive" src="images/thumbs/8.jpg" alt="img01"/>
-						</a>
-					</li>
-					 <div class="clearfix"> </div>
-					</ul>
+			 <div class="section group">
+				<div class="col_1_of_4 span_1_of_4">
+					<a href="images/1/5.jpg" class="swipebox" title=""> <img src="images/1/thumbs/5.jpg" alt=""><span class="zoom-icon"></span> </a>
+
+                </div>
+				<div class="col_1_of_4 span_1_of_4">
+					<a href="images/1/6.jpg" class="swipebox" title=""> <img src="images/1/thumbs/6.jpg" alt=""><span class="zoom-icon"></span> </a>
+
+                </div>
+				<div class="col_1_of_4 span_1_of_4">
+					<a href="images/1/7.jpg" class="swipebox" title=""> <img src="images/1/thumbs/7.jpg" alt=""><span class="zoom-icon"></span> </a>
+
+                </div>
+				<div class="col_1_of_4 span_1_of_4">
+					<a href="images/1/8.jpg" class="swipebox" title=""> <img src="images/1/thumbs/8.jpg" alt=""><span class="zoom-icon"></span> </a>
+
+                </div>
+				<div class="clear"></div>
 			</div>
 		</div>
+	 </div>
 	</div>
-	<!----end-gallery---->
-	<script src="js/grid.js"></script>
-		<script>
-			$(function() {
-				Grid.init();
-			});
-		</script>
+	<!----end-blog---->
 	<!----start-footer---->
-	<div class="footer">
-		<div class="container">
-			<div class="footer-top">
-				<ul>
-					<li><a href="#"><span class="fb"> </span></a></li>
-					<li><a href="#"><span class="twit"> </span></a></li>
-					<li><a href="#"><span class="google"> </span></a></li>
-					<li><a href="#"><span class="pin"> </span></a></li>
-					<li><a href="#"><span class="dribble"> </span></a></li>
-				</ul>
-			</div>
-			<div class="newsletter">
-				<ul>
-					<li><h6>Cadastre-se  :</h6></li>
-					<li><input type="text" value="" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = '';}"/></li>
-					<li><form><input type="submit" value="Enviar" /></form></li>
-				</ul>
-			</div>
-			<div class="footer-text">
-				<p>Design by <a href="http://hcjweb.com.br/" target="_blank"> hcjweb</a></p>
-			</div>
-		</div>
-		<script type="text/javascript">
-									$(document).ready(function() {
-										/*
-										var defaults = {
-								  			containerID: 'toTop', // fading element id
-											containerHoverID: 'toTopHover', // fading element hover id
-											scrollSpeed: 1200,
-											easingType: 'linear'
-								 		};
-										*/
 
-										$().UItoTop({ easingType: 'easeOutQuart' });
+<?php include 'inc/footer.php'; ?>
 
-									});
-								</script>
-		<a href="#home" id="toTop" class="scroll" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
-	</div>
 	<!----end-footer---->
 </body>
 </html>
